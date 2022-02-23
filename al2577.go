7a00762120a4832a1e04f794b2d3a635f3ca6b83
ysun7 <ysun7@beautifulstore.org>
2022-02-23 21:33:48 +0900

    #1 2022-02-23 백준 2577번 알고리즘

---
+++ al2577.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Al2577(a int, b int, c int)  {
+	num := a * b * c
+	str := strconv.Itoa(num)
+	var slice []string
+	r := [10]int{}
+
+	for i:=0; i< len(str) ; i++ {
+		slice = append(slice, str[i:i+1])
+		b, _ := strconv.Atoi(slice[i])
+		r[b] +=1
+	}
+
+	for i:=0; i<len(r); i++ {
+		fmt.Println(r[i])
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
 func main()  {
-	
+	Al2577(150,266,427)
 }
