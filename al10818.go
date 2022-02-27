9680bce0eb65d701950af4836bdc1b50867a251c
ysun7 <ysun7@beautifulstore.org>
2022-02-27 22:46:05 +0900

    #3 2022-02-27 백준 10818번 알고리즘

--- al10818.go
+++ al10818.go
@@ -4,23 +4,35 @@ import (
 	"fmt"
 )
 
-func Al10818() {
+func Al10818() error {
 	var n int
-	var str,min,max int
-	fmt.Scanln(&n)
-	for i := 0; i < n; i++ {
-		fmt.Scanf("%d", &str)
-			if i==0 {
-				min =str
-				max =str
-			}
-			if min > str {
-				min = str
-			}
-			if max < str {
-				max = str
-			}
+	var str, min, max int
+	_, err := fmt.Scan(&n)
+	if n <= 1 && n >= 1000000 {
+		return err
 	}
-	fmt.Println(min, max)
+	var arr []int
+	fmt.Println(n)
+		for i := 0; i < n; i++ {
+			_, err = fmt.Scanf("%d", &str)
+			arr = append(arr, str)
+			if str <= -1000000 && str >= 1000000 {
+				return err
+			}
 
+		}
+		fmt.Println(arr)
+		min = arr[0]
+		max = arr[0]
+		for i := 0; i < n; i++ {
+			if min > arr[i] {
+				min = arr[i]
+			}
+			if max < arr[i] {
+				max = arr[i]
+			}
+		}
+
+	fmt.Println(min, max)
+	return 	nil
 }
