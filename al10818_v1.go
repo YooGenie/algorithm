1a681a0388d9708651c9a8b7c72ed30e208ce8b8
ysun7 <ysun7@beautifulstore.org>
2022-03-01 21:24:57 +0900

    #3 2022-03-01 백준 10818번 알고리즘 v3,v4

---
+++ al10818_v1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Al10818V1() {
+	var n,str, min, max int
+	fmt.Scanln(&n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &str)
+		if i==0 {
+			min =str
+			max =str
+		}
+		if min > str {
+			min = str
+		}
+		if max < str {
+			max = str
+		}
+	}
+	fmt.Println(min, max)
+
+}
---
+++ al10818_v4.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Al10818V4(){
+	stdin := bufio.NewReader(os.Stdin)
+	var n, min, max int
+	_, err := fmt.Scan(&n)
+	arr := make([]int, n)
+	if err!= nil {
+		fmt.Println("error")
+		stdin.ReadString('\n')
+	}
+	for i := 0; i < n; i++ {
+		_, err = fmt.Scanf("%d", &arr[i])
+		if err!= nil {
+			fmt.Println("error")
+			stdin.ReadString('\n')
+		}
+	}
+	min = arr[0]
+	max = arr[0]
+	for i := 0; i < n; i++ {
+		if min > arr[i] {
+			min = arr[i]
+		}
+		if max < arr[i] {
+			max = arr[i]
+		}
+	}
+	fmt.Println(min, max)
+}
