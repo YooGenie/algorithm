c59df58e322637f8e785d111cca989c4193c394c
ysun7 <ysun7@beautifulstore.org>
2022-03-03 16:56:01 +0900

    #9 2022-03-03 백준 9498번 알고리즘

---
+++ al9498.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func Al9498()  {
+	var a int
+	fmt.Scanln(&a)
+	if a <=100 && a>=90 {
+		fmt.Println("A")
+	}else if a <90 && a>=80 {
+		fmt.Println("B")
+	}else if a <80 && a>=70 {
+		fmt.Println("C")
+	}else if a <70 && a>=60 {
+		fmt.Println("D")
+	}else {
+		fmt.Println("F")
+	}
+}
+
