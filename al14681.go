2dacaa53a3e8e09d1cfccd1baaa1a87ff56cc563
ysun7 <ysun7@beautifulstore.org>
2022-03-06 21:53:10 +0900

    #13 2022-03-06 백준 14681번 알고리즘

---
+++ al14681.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func Al14681() {
+	var x, y, result int
+	fmt.Scanln(&x)
+	fmt.Scanln(&y)
+
+	if x > 0 && y > 0 {
+		result = 1
+	} else if x < 0 && y > 0 {
+		result = 2
+	} else if x < 0 && y < 0 {
+		result = 3
+	} else if x > 0 && y < 0 {
+		result = 4
+	}
+	fmt.Println(result)
+}
