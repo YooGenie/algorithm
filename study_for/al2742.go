21b2f04c01ed6edd2f8c8fd09ce386d645d729a0
ysun7 <ysun7@beautifulstore.org>
2022-03-23 21:59:46 +0900

    #33 2022-03-24 백준 11021번 알고리즘

---
+++ study_for/al2742.go
@@ -0,0 +1,19 @@
+package study_for
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Al2742() {
+	var a int
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	fmt.Fscan(reader,&a)
+	for i := a; i >= 1; i-- {
+		fmt.Fprintln(writer,i)
+	}
+}
