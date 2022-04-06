09c8f6d8e0c004bdf0c38adb688386b344f31f0b
ysun7 <ysun7@beautifulstore.org>
2022-04-06 09:27:22 +0900

    #55 2022-04-06 프로그래머스 평균 구하기

---
+++ programmers/al12944.go
@@ -0,0 +1,18 @@
+package programmers
+
+import (
+	"fmt"
+)
+
+func Al12944() {
+	var arr = []int{ 1,2,3,4}
+	var sum int
+	var result float64
+
+	for i:=0; i< len(arr) ; i++ {
+		sum += arr[i]
+	}
+
+	result= float64(sum) / float64(len(arr))
+	fmt.Println(result)
+}
