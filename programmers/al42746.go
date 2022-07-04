50b31177953bdde286b0c64c9d8ae71587e48c6e
ysun7 <ysun7@beautifulstore.org>
2022-07-04 20:52:17 +0900

    #111 2022-05-16 프로그래머스 가장 큰 수

--- programmers/al42746.go
+++ programmers/al42746.go
@@ -4,39 +4,49 @@ import (
 	"fmt"
 	"sort"
 	"strconv"
-	"strings"
 )
 
 func Al42746() {
 	var numbers = []int{3, 30, 34, 5, 9}
 
+
+
+
 	var str []string
 	var nums []int
+
 
 	for _, v := range numbers {
 		str = append(str, strconv.Itoa(v))
 	}
 	sort.Sort(sort.Reverse(sort.StringSlice(str)))
 
-
 	for _, v := range str {
 		num ,_ := strconv.Atoi(v)
 		nums = append(nums, num)
 	}
 
-	for i:=0 ; i<len(nums); i++ {
-		for j:=i+1 ;j<len(nums); j++  {
-			if str[i][0] == str[j][0] && nums[i]%10 < nums[j]%10{
-				sort.StringSlice(str).Swap(i, j)
-				sort.IntSlice(nums).Swap(i, j)
-			}
+	temp :=0
+	for i:=0 ; i<len(nums)-1; i++ {
+		for j:=i+1 ;j<len(nums); j++ {
+			if nums[i]/10*len(str[i]) == nums[j]/10*len(str[j]) {
+				if nums[i]%10*len(str[i]) < nums[j]%10*len(str[j]) {
+					temp = nums[i]
+					nums[i] = nums[j]
+					nums[j] = temp
+				}
+			} else if nums[i]%10*len(str[i]) == nums[j]/10*len(str[j]) && nums[i]%10*len(str[i]) < nums[j]%10*len(str[j]) {
+					temp = nums[i]
+					nums[i] = nums[j]
+					nums[j] = temp
+				}
 		}
-
 	}
-
-
-
-	result := strings.Join(str, "")
+	result:=""
+	for _, v := range nums {
+		result += strconv.Itoa(v)
+	}
+	//result := strings.Join(nums, "")
 	fmt.Println(result)
 }
 
